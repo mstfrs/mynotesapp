c6ddc5c26c22380f989803560e3030b59cb860a2
mstfrs <mstfrs@gmail.com>
2022-10-25 20:56:30 +0300

    map error

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -148,7 +148,7 @@ const InputArea = () => {
           />
         </div>
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 place-self-center w-1/2 lg:w-full md:w-full">
-          {(filterednotes.length === 0 ?(notes.length!==0?notes:null): filterednotes).map((note) => (
+          {notes===null?null:(filterednotes.length === 0 ?notes: filterednotes).map((note) => (
             <div
               key={note.id}
               className=" items-center object-center self-center content-center m-auto w-full"
